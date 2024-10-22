36de5b82d53c0ab5dc501f4ed5969c1f80cc0ac5
Muhammad Ariva <90445234+ArivaAR@users.noreply.github.com>
2024-10-22 15:53:21 +0800

    Delete Menu.php

--- Menu.php
+++
@@ -1,9 +0,0 @@
-<main class="pilih-main">
-                <a href="#" class="pilih-option">Pengunjung</a>
-                <a href="#" class="pilih-option">Narasumber</a>
-                <a href="#" class="pilih-option">Magang</a>
-            </main>
-
-        <button type="button" class="btn">Pengunjung</button>
-        <button type="button" class="btn">Narasumber</button>
-        <button type="button" class="btn">Magang</button>
