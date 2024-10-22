b43815e80b800164603bbe384b198daef3f3d89f
Muhammad Ariva <90445234+ArivaAR@users.noreply.github.com>
2024-10-22 12:04:48 +0800

    22/10/2024 12:04

---
+++ Footer.php
@@ -0,0 +1,7 @@
+<nav class="navbar fixed-bottom bg-body-tertiary">
+    <div class="container-fluid d-flex justify-content-center align-items-end">
+    <footer class="pilih-footer">
+                <p>&copy; COPYRIGHT TVRI</p>
+            </footer>
+    </div>
+</nav>
