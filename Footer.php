0cbaa2c67326d637c44efe1e570706360469624d
Muhammad Ariva <90445234+ArivaAR@users.noreply.github.com>
2024-10-22 15:52:54 +0800

    Delete Footer.php

--- Footer.php
+++
@@ -1,7 +0,0 @@
-<nav class="navbar fixed-bottom bg-body-tertiary">
-    <div class="container-fluid d-flex justify-content-center align-items-end">
-    <footer class="pilih-footer">
-                <p>&copy; COPYRIGHT TVRI</p>
-            </footer>
-    </div>
-</nav>
